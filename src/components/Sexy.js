22127c16272f243c630049993bbc99185912a37d
MKAVARTS <mkavarts@gmail.com>
2018-04-27 11:40:13 -0500

    push files

---
+++ src/components/Sexy.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Jumbotron, Button } from 'reactstrap';
+
+const Sexy = (props) => {
+  return (
+    <div>
+      <Jumbotron>
+        <h1 className="display-3">SESSY TIME!</h1>
+      </Jumbotron>
+    </div>
+  );
+};
+
+export default Sexy;
